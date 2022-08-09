d8d5fdb3fbe836099abbb4d653d4b05a23248bf5
yr-lee <84362234+yr-lee@users.noreply.github.com>
2022-08-09 15:20:00 +0900

    table

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 import React, { Component } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
@@ -6,24 +13,24 @@ const customers = [
   {
     id: 1,
     image: "https://placeimg.com/64/64/any",
-    name: "홍길동",
-    birthday: "000000",
+    name: "홍길동1",
+    birthday: "111111",
     gender: "남자",
     job: "대학생",
   },
   {
     id: 2,
     image: "https://placeimg.com/64/64/any",
-    name: "홍길동",
-    birthday: "000000",
+    name: "홍길동2",
+    birthday: "222222",
     gender: "남자",
     job: "대학생",
   },
   {
     id: 3,
     image: "https://placeimg.com/64/64/any",
-    name: "홍길동",
-    birthday: "000000",
+    name: "홍길동3",
+    birthday: "333333",
     gender: "남자",
     job: "대학생",
   },
@@ -32,19 +39,33 @@ class App extends Component {
   render() {
     return (
       <div>
-        {customers.map((c) => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })}
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => {
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
       </div>
     );
   }
