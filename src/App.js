aba231df1cfa225e62b76b8ae18fc5f624cd54b2
yr-lee <84362234+yr-lee@users.noreply.github.com>
2022-08-09 13:31:34 +0900

    chap4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="bg-black">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>React Management System</h1>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any",
+    name: "홍길동",
+    birthday: "000000",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/any",
+    name: "홍길동",
+    birthday: "000000",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/any",
+    name: "홍길동",
+    birthday: "000000",
+    gender: "남자",
+    job: "대학생",
+  },
+];
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
